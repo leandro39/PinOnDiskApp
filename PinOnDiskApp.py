0ac5b1b399cd9901646cb42e8dcf20255fc6e92e
Leandro Londra <leanlondra@gmail.com>
2020-06-20 00:33:12 -0300

    Plotter added and more bug fixes, safe thread exit

--- PinOnDiskApp.py
+++ PinOnDiskApp.py
@@ -17,11 +17,17 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 from matplotlib.figure import Figure
-import random
 import numpy as np
 
 ICON_RED_LED = '.\\views\\icons\\led-red-on.png'
 ICON_GREEN_LED = '.\\views\\icons\\green-led-on.png'
+
+#Load configs
+with open('configs.json', 'r') as f:
+    configs = json.loads(f.read())
+    TEST_ENV = configs['TEST_ENV']
+    BUFFER_SIZE = configs['BUFFER_SIZE']
+    COMPORT_CELDA = configs['COMPORT_CELDA']
 
 class PinOnDiskApp(QtWidgets.QMainWindow):
     def __init__(self):
@@ -29,11 +35,6 @@ class PinOnDiskApp(QtWidgets.QMainWindow):
 
         #Create main window
         self.ui = Ui_MainWindow()
-
-        #Load configs
-        with open('configs.json', 'r') as f:
-            self.configs = json.loads(f.read())
-
         self.dialogs = list()           
         #Setup the ui
         self.ui.setupUi(self)
@@ -51,7 +52,6 @@ class PinOnDiskApp(QtWidgets.QMainWindow):
         self.ui.distanciaInput.setValidator(self.validator)
         self.ui.cargaInput.setValidator(self.validator)
 
-        #Flow control variables
         self.isConnected = False
         self.ser = serial.Serial()
                 
@@ -68,8 +68,7 @@ class PinOnDiskApp(QtWidgets.QMainWindow):
         
     #Button events
     def conectarBtn_ClickedEvent(self):
-        # plot = Plotter()
-        # plot.show()
+
         if not self.isConnected:
             self.isConnected = serial_tools.try_connect(self.ser, self.ui.portCombo.currentText())
         else:
@@ -77,6 +76,7 @@ class PinOnDiskApp(QtWidgets.QMainWindow):
         
         if self.isConnected:
             self.ui.groupBox.setEnabled(True)
+            self.ui.testBtn.setEnabled(True)
             self.ui.labelNotConnected.hide()
             self.ui.labelConnected.show()
             self.ui.conectarBtn.setText("Desconectar")
@@ -101,7 +101,7 @@ class PinOnDiskApp(QtWidgets.QMainWindow):
                 
     def startBtn_ClickedEvent(self):
         #Creo nuevo ensayo
-        self.ensayo = Ensayo(self.ui.experimentNameInput.text(), self.ui.distanciaInput.text(), self.ui.radioCombo.currentText(), self.ui.cargaInput.text(), self.configs['COMPORT_CELDA'], self.ser)
+        self.ensayo = Ensayo(self.ui.experimentNameInput.text(), self.ui.distanciaInput.text(), self.ui.radioCombo.currentText(), self.ui.cargaInput.text(), COMPORT_CELDA, self.ser, TEST_ENV)
         self.ensayo.setSavePath(self.pathParser())
         self.ui.pauseBtn.setEnabled(False)
         self.ui.stopBtn.setEnabled(True)
@@ -113,15 +113,22 @@ class PinOnDiskApp(QtWidgets.QMainWindow):
         self.ui.pathBrowseBtn.setEnabled(False)
         self.ui.conectarBtn.setEnabled(False)
         self.ui.portCombo.setEnabled(False)
+        self.ui.testBtn.setEnabled(False)
         self.ensayo.empezar()
         self.ensayo.experimentEnd.connect(self.onExperimentEnd)
+        
+        # Plotter setup
+        self.plot = Plotter(ensayo = self.ensayo, test = self.ui.experimentNameInput.text(), r = self.ui.radioCombo.currentText(), d = self.ui.distanciaInput.text(), c = self.ui.cargaInput.text())
+        self.plot.show()
+
+        # Progress bar setup
         self.ui.progressBar.reset()
         self.ui.progressBar.setMaximum(int(self.ensayo.getVueltasTarget()))
         self.progress = ProgressBarUpdater(self.ensayo.progressBarQ)
         self.progress.currentVueltas.connect(self.onVueltasChanged)
         self.ui.progressLabel.setVisible(True)
         self.progress.start()
-    
+        
     def stopBtn_ClickedEvent(self):
         confirm = QtWidgets.QMessageBox.question(self,'Detener ensayo', "¿Está seguro que desea detener el experimento?",QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No, QtWidgets.QMessageBox.No)
         if confirm == QtWidgets.QMessageBox.Yes:
@@ -134,18 +141,21 @@ class PinOnDiskApp(QtWidgets.QMainWindow):
         return self.ui.pathInput.text().replace('/', '\\')
 
     def testBtn_ClickedEvent(self):
-        text = self.ui.testBtn.text()
-        if text == "Prueba":
-            self.ensayo.test()
+        testing = Ensayo.test(self.ser, TEST_ENV)
+        if (testing):
+            # Test started
+            self.ui.testBtn.setText('Detener prueba')
         else:
-            self.ensayo.detener()
+            self.ui.testBtn.setText('Prueba')
+        
 
     def onVueltasChanged(self, value):
         self.ui.progressBar.setValue(int(value))
         self.ui.progressLabel.setText('{currVueltas} vueltas de {targetVueltas}'.format(currVueltas=int(value), targetVueltas=int(self.ensayo.getVueltasTarget())))
 
     def onExperimentEnd(self):
-        print("hello from end event")
+         #Rutina de limpieza para volver a empezar nuevo experimento
+
         self.ui.pauseBtn.setEnabled(False)
         self.ui.stopBtn.setEnabled(False)
         self.ui.startBtn.setEnabled(True)
@@ -156,41 +166,54 @@ class PinOnDiskApp(QtWidgets.QMainWindow):
         self.ui.pathBrowseBtn.setEnabled(True)
         self.ui.conectarBtn.setEnabled(True)
         self.ui.portCombo.setEnabled(True)
+        self.ui.testBtn.setEnabled(True)
         self.ui.progressBar.setValue(self.ui.progressBar.maximum())
         self.ui.progressLabel.setText('{targetVueltas} vueltas de {targetVueltas}'.format(targetVueltas=int(self.ensayo.getVueltasTarget())))
-        self.ui.progressBar.killThread = True
-        #Rutina de limpieza para volver a empezar nuevo experimento
-        #Agregar señal en start button y conectarla con este método
-        #Emitir señal desde experiment.py
+        self.progress.killThread = True
+        self.ser.flushInput()
+        self.ser.flushOutput()
         
+        
+        # ONLY DEBUG
+        # time.sleep(6)
+        # print(f'Celda thread is alive: {self.ensayo.t1.is_alive()}')
+        # print(f'Controller thread is alive: {self.ensayo.t2.is_alive()}')
+        # print(f'Data writer thread: {self.ensayo.t3.is_alive()}')
+        # print(f'Temp y humedad thread is alive : {self.ensayo.t6.is_alive()}')
+        # print(f'Plotter thread is alive: {self.plot.t.is_alive()}')
+        # print(f'Progress bar updater thread is alive: {self.progress.isRunning()}')
+
 
 class ProgressBarUpdater(QtCore.QThread):
     currentVueltas = QtCore.pyqtSignal(float)
-    killThread = False
+    
     def __init__(self,Q, parent=None):
         super(QtCore.QThread, self).__init__()
         self.vueltasQueue = Q
+        self.killThread = False
     
     def run(self):
         while True:
-            if (self.killThread):
-                break
-            vueltas = self.vueltasQueue.get()
-            self.currentVueltas.emit(int(vueltas))
-            
+            try:
+                if (self.killThread):
+                    break
+                vueltas = self.vueltasQueue.get(timeout=1.5)
+                self.currentVueltas.emit(int(vueltas))
+            except queue.Empty:
+                pass
 
 class Plotter(QtWidgets.QDialog):
-    def __init__(self, ensayo = None, parent=None):
+    def __init__(self, r, d, c, ensayo = None, test = 'Experimento', parent=None):
         super(Plotter, self).__init__(parent)
         self.setWindowFlags((self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint) | QtCore.Qt.WindowMinimizeButtonHint | QtCore.Qt.WindowMaximizeButtonHint)
         self.setWindowTitle("Ensayo")
         self.setStyleSheet("QWidget { background-color: #ffffff; }")
         self.canvas = FigureCanvas(Figure(figsize=(10,6)))
         self.toolbar = NavigationToolbar(self.canvas, self)
-        self.toolbar.setVisible(False)
+        self.toolbar.setVisible(True)
         self._dynamic_ax = self.canvas.figure.subplots()
         self._dynamic_ax.grid()
-        self._dynamic_ax.set_title('Ensayo: Agos genia', pad=15)
+        self._dynamic_ax.set_title('Ensayo: {name} - Radio: {radio}mm - Distancia: {dist}m - Carga: {carga}N'.format(name=test, radio=r, dist=d, carga=c), pad=15)
         self._dynamic_ax.set_xlabel('Distancia [m]', labelpad=15)
         self._dynamic_ax.set_ylabel('Fuerza de rozamiento [kg]', labelpad=20)
         self._dynamic_ax.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
@@ -201,40 +224,56 @@ class Plotter(QtWidgets.QDialog):
         layout.addWidget(self.canvas)
         self.setLayout(layout)
 
-        t = Thread(target=self._update_canvas, daemon=True)
-        t.start()
+        self.ensayo = ensayo
+        self.t = Thread(target=self._update_canvas, daemon=True)
+        self.t.start()
 
     def _update_canvas(self):
         
-        f = open('data\\test1.csv', 'r')
-        cs = csv.reader(f)
-        lines = next(f).split(',')
+        # f = open('data\\test1.csv', 'r')
+        # cs = csv.reader(f)
+        # lines = next(f).split(',')
         
-        self._dynamic_ax.plot(float(lines[1]), float(lines[0]))
+        # self._dynamic_ax.plot(float(lines[1]), float(lines[0]))
+        # self._dynamic_ax.figure.canvas.draw()
+        # line = self._dynamic_ax.get_lines()[0]
+        # for row in cs:
+        #     line.set_xdata(np.append(line.get_xdata(),(float(row[1]))))
+        #     line.set_ydata(np.append(line.get_ydata(),(float(row[0]))))
+        #     self._dynamic_ax.set_ylim(0,np.max(line.get_ydata()))
+        #     self._dynamic_ax.set_xlim(0,np.max(line.get_xdata()))
+        #     self._dynamic_ax.figure.canvas.draw()
+        #     time.sleep(0.4)
+        # f.close()
+
+        
+        data = self.ensayo.plotterQ.get()
+        self._dynamic_ax.plot(float(data[1]), float(data[0]))
         self._dynamic_ax.figure.canvas.draw()
         line = self._dynamic_ax.get_lines()[0]
-        for row in cs:
-            line.set_xdata(np.append(line.get_xdata(),(float(row[1]))))
-            line.set_ydata(np.append(line.get_ydata(),(float(row[0]))))
-            self._dynamic_ax.set_ylim(0,np.max(line.get_ydata()))
-            self._dynamic_ax.set_xlim(0,np.max(line.get_xdata()))
-            self._dynamic_ax.figure.canvas.draw()
-            time.sleep(0.4)
-        f.close()
+        while True:
+            try:     
+                data = self.ensayo.plotterQ.get(timeout=1.5)
+                if data == 'end':
+                    break
+                else:
+                    xdata = line.get_xdata()
+                    ydata = line.get_ydata()
+                    if (line.get_xdata().size >= BUFFER_SIZE):
+
+                        # Left shift array to move x axis once BUFFER_SIZE is reached
+                        line.set_xdata(np.append(xdata, float(data[1]))[1:])
+                        line.set_ydata(np.append(ydata, float(data[0]))[1:])
+                    else:
+                        line.set_xdata(np.append(xdata, float(data[1])))
+                        line.set_ydata(np.append(ydata, float(data[0])))
+                    
+                    self._dynamic_ax.set_ylim(np.min(line.get_ydata())*0.9,np.max(line.get_ydata())*1.1)
+                    self._dynamic_ax.set_xlim(np.min(line.get_xdata()),np.max(line.get_xdata()))
+                    self._dynamic_ax.figure.canvas.draw()
         
-        
-        # while True:
-        #     data = self.ensayo.plotterQ.get()
-        #     if data == 'end':
-        #         break
-        #     else:
-        #         self._dynamic_ax.set
-        #self._dynamic_ax.grid()
-        #t = np.linspace(0, 10, 101)
-        # Shift the sinusoid as a function of time.
-        #self._dynamic_ax.plot(t, np.sin(t + time.time()))
-        #self._dynamic_ax.set_xlim(left=0)
-        #self._dynamic_ax.figure.canvas.draw()
+            except queue.Empty:
+                pass
 
 
 if __name__ == "__main__":
--- controller/experiment.py
+++ controller/experiment.py
@@ -6,14 +6,13 @@ from math import pi
 from threading import Event, Lock, Thread
 import serial
 from PyQt5 import QtCore
-
-TEST_ENV = True
+from PyQt5.QtWidgets import QMessageBox
 
 class Ensayo(QtCore.QObject):
     
     experimentEnd = QtCore.pyqtSignal()
 
-    def __init__(self, name, dist, radio, carga, puertoCelda, serialArduino, parent=None):
+    def __init__(self, name, dist, radio, carga, puertoCelda, serialArduino, TEST_ENV, parent=None):
         super(self.__class__, self).__init__(parent)
         
         self._name = name
@@ -26,6 +25,7 @@ class Ensayo(QtCore.QObject):
         self._serialCelda = serial.Serial()
         self.isRunning = False
         self._stopThreads = False
+        self.TEST_ENV = TEST_ENV
         
 
     def getDistancia(self):
@@ -61,138 +61,168 @@ class Ensayo(QtCore.QObject):
             self._tyhQ = queue.Queue()
             self._dataEvent = Event()
             self._dataReady = Event()
+            self._stopExperiment = False
             self._lock = Lock()
             
             #Empty buffers
             self._serialArduino.flushInput()
             self._serialArduino.flushOutput()
-            #Added \n, delete later
-            if (TEST_ENV):
+            
+            if (self.TEST_ENV):
                 self._serialArduino.write(('<STAR,{radio},{vueltasTarget}>\n'.format(radio=self._radio, vueltasTarget=self._vueltasTarget)).encode())
             else:
                 self._serialArduino.write(('<STAR,{radio},{vueltasTarget}>'.format(radio=self._radio, vueltasTarget=self._vueltasTarget)).encode())
             
             response = self._serialArduino.readline().decode('ascii').strip()
-            print(response)
+            
             if not (response == "0"):
                 raise Exception('Error en comunicacion con controlador')
             self.isRunning = True
             self._t0 = time.time()  #Timestamp de comienzo de experimento
             
             #Listeners de puerto serial y producers de queues
-            t1 = Thread(target=self.__celdaListener, daemon=True)
-            t2 = Thread(target=self.__controllerListener, daemon=True)
-            t6 = Thread(target=self.__requestTemperatureAndHumidity, daemon=True)
-            #Consumer de queues y almacenador
-            t3 = Thread(target=self.__dataWriter, daemon=True)
-            t6.start()
-            t1.start()
-            t2.start()
-            t3.start()
+            self.t1 = Thread(target=self.__celdaListener, daemon=True)
+            self.t2 = Thread(target=self.__controllerListener, daemon=True)
+            self.t6 = Thread(target=self.__requestTemperatureAndHumidity, daemon=True)
+            
+            #Consumer de queues y data writer
+            self.t3 = Thread(target=self.__dataWriter, daemon=True)
+            self.t6.start()
+            self.t1.start()
+            self.t2.start()
+            self.t3.start()
 
         except Exception as e:
-            print(type (e))
-            print(e.args)
-            print(e)
+            msgBox = QMessageBox()
+            msgBox.setIcon(QMessageBox.Critical)
+            msgBox.setText('Error: ' + str(type(e)) + '\n'  + str(e))
+            msgBox.setWindowTitle('Error')
+            msgBox.exec_()
 
     def pausar(self):
         pass
 
     def detener(self):
-        t5 = Thread(target=self.__stopExperiment, daemon=True)
-        t5.start()
+        self._stopExperiment = True #Raise flag to stop experiment in a clean way
     
-    def test(self):
-        t = Thread(target=self.__test, daemon=True)
-        t.start()
-    
-    def __test(self):
+    @staticmethod
+    def test(port, TEST_ENV):
         try:
-            self._lock.acquire()
             if (TEST_ENV):
-                self._serialArduino.write('<TEST>').encode()
+                port.write(b'<TEST>\n')
             else:    
-                self._serialArduino.write('<TEST>\n').encode()
-            answer = self._serialArduino.readline().decode('ascii').strip()
-            self._lock.release()
+                port.write(b'<TEST>')
+            
+            answer = port.readline().decode('ascii').strip()
+            print(answer)
             if (answer == '0'):
-                self.isRunning = True
-                pass
+                return 1
+            
+            elif (answer == '-1'):
+                return 0    
             else:
                 raise Exception('Error de comunicacion')
             
         except Exception as e:
-            print(type(e))
-            print(e.args)
-            print(e)
+            msgBox = QMessageBox()
+            msgBox.setIcon(QMessageBox.Critical)
+            msgBox.setText('Error: ' + str(type(e)) + '\n'  + str(e))
+            msgBox.setWindowTitle('Error')
+            msgBox.exec_()
     
-    #ADDED \N REMOVE LATER
+    
     def __stopExperiment(self):
-        if (self.isRunning):
-            self._lock.acquire()
-            if (TEST_ENV):
-                self._serialArduino.write(b'<STOP>\n')
-            else:
-                self._serialArduino.write(b'<STOP>')
-            answer = self._serialArduino.readline().decode('ascii').strip()
-            self._lock.release()
-            
-            if (answer == "-1"):
-                print(answer)
+        try:
+            if (self.isRunning):
                 self._lock.acquire()
-                self._stopThreads = True
+                if (self.TEST_ENV):
+                    self._serialArduino.write(b'<STOP>\n')
+                else:
+                    self._serialArduino.write(b'<STOP>')
+                answer = self._serialArduino.readline().decode('ascii').strip()
                 self._lock.release()
-                self.isRunning = False
+                if (answer == "-1"):
+                    print(answer)
+                    self._lock.acquire()
+                    self._stopThreads = True
+                    self._lock.release()
+                    self.isRunning = False
+        
+        except Exception as e:
+            msgBox = QMessageBox()
+            msgBox.setIcon(QMessageBox.Critical)
+            msgBox.setText('Error: ' + str(type(e)) + '\n'  + str(e))
+            msgBox.setWindowTitle('Error')
+            msgBox.exec_()
   
     def __celdaListener(self):
-        while True:
-            if self._stopThreads:
-                self._serialCelda.close()
-                break
-            celda = self._serialCelda.readline()
-            if not (celda == b""):
-                if (TEST_ENV):
-                    celda = celda.decode('ascii').strip()[8:13]
-                else:
-                    celda = celda.decode('ascii').strip()[4:9]  #Parse poco prolijo para test
-                self._celdaQ.put(celda)  
-                self._dataEvent.set()
-            
-    def __controllerListener(self):
-        while True:
-            if self._stopThreads:
-                self.experimentEnd.emit()
-                break
-            self._dataEvent.wait(1.5)
-            if (self._dataEvent.isSet() and not self._celdaQ.empty()):
-                self._lock.acquire()
-                if (TEST_ENV):
-                    self._serialArduino.write(b'<SEND>\n')
-                else:
-                    self._serialArduino.write(b'<SEND>')
-                answer = self._serialArduino.readline().decode('ascii').strip()
-                print(answer)
-                self._lock.release()
-                if (answer == "1"):
-                    self._stopThreads = True
-                    self._dataEvent.clear()
-                    self.isRunning = False
-                    self.experimentEnd.emit()
-                    print("Experimento terminado")
+        try:
+            while True:
+                if self._stopThreads:
+                    self._serialCelda.close()
                     break
-                else:
-                    vueltas = float(answer)
-                    self.progressBarQ.put(vueltas)
-                    self._distQ.put("{:10.2f}".format(vueltas*2*pi*self._radio*0.001).strip())
-                    self._dataEvent.clear()
-                    self._dataReady.set()
-                              
+                celda = self._serialCelda.readline()
+                if not (celda == b""):
+                    if (self.TEST_ENV):
+                        celda = celda.decode('ascii').strip()[8:13]
+                    else:
+                        celda = celda.decode('ascii').strip()[4:9]  #Parse poco prolijo para test
+                    self._celdaQ.put(celda)  
+                    self._dataEvent.set()
+        
+        except Exception as e:
+            msgBox = QMessageBox()
+            msgBox.setIcon(QMessageBox.Critical)
+            msgBox.setText('Error: ' + str(type(e)) + '\n'  + str(e))
+            msgBox.setWindowTitle('Error')
+            msgBox.exec_()
+
+    def __controllerListener(self):
+        try:
+            while True:
+                if self._stopThreads:
+                    self.experimentEnd.emit()
+                    break
+                self._dataEvent.wait(1.5)
+                if (self._dataEvent.isSet() and not self._celdaQ.empty()):
+                    self._lock.acquire()
+                    if (self.TEST_ENV):
+                        self._serialArduino.write(b'<SEND>\n')
+                    else:
+                        self._serialArduino.write(b'<SEND>')
+                    answer = self._serialArduino.readline().decode('ascii').strip()
+                    self._lock.release()
+                    if (answer == "1"):
+                        self._stopThreads = True
+                        self._dataEvent.clear()
+                        self.isRunning = False
+                        self.experimentEnd.emit()
+                        break
+                    else:
+                        vueltas = float(answer)
+                        self.progressBarQ.put(vueltas)
+                        self._distQ.put("{:10.2f}".format(vueltas*2*pi*self._radio*0.001).strip())
+                        self._dataEvent.clear()
+                        self._dataReady.set()
+                    
+                    if (self._stopExperiment == True):
+                        self.__stopExperiment()
+        
+        except Exception as e:
+            msgBox = QMessageBox()
+            msgBox.setIcon(QMessageBox.Critical)
+            msgBox.setText('Error: ' + str(type(e)) + '\n'  + str(e))
+            msgBox.setWindowTitle('Error')
+            msgBox.exec_()
+
     def __dataWriter(self):
         try:
+            # Create output file
             self._out = open(self._path + '\\' + self._name + '.txt', 'w', newline='')
             csv_out = csv.writer(self._out, delimiter='\t')
             
-            #Write metada
+            # Write metada
+
             localtime = time.localtime()
             today = time.strftime('%d/%m/%y', localtime)
             now = time.strftime('%H:%M:%S', localtime)
@@ -211,55 +241,57 @@ class Ensayo(QtCore.QObject):
             self._out.flush()
 
             while True:
-                if self._stopThreads:
-                    self.plotterQ.put('end')   
-                    break     
-                self._dataReady.wait(1.5)
-                if (self._dataReady.isSet() and not (self._celdaQ.empty() and self._distQ.empty())):
-                    timestamp = time.time()-self._t0
-                    self._dataReady.clear()
-                    celda = self._celdaQ.get()
-                    distancia = self._distQ.get()
-                    self.plotterQ.put((celda, distancia))
-                    if(self._tyhQ.empty()):
-                        csv_out.writerow([celda, distancia,'{:10.2f}'.format(timestamp).strip(),self.tempHumedad[0], self.tempHumedad[1]])
+                try:
+                    if self._stopThreads:
+                        self.plotterQ.put('end')   
+                        break     
+                    self._dataReady.wait(1.5)
+                    if (self._dataReady.isSet() and not (self._celdaQ.empty() and self._distQ.empty())):
+                        timestamp = time.time()-self._t0
+                        self._dataReady.clear()
+                        celda = self._celdaQ.get(timeout=1.5)
+                        distancia = self._distQ.get(timeout=1.5)
+                        self.plotterQ.put((celda, distancia))
+                        if(self._tyhQ.empty()):
+                            csv_out.writerow([celda, distancia,'{:10.2f}'.format(timestamp).strip(),self.tempHumedad[0], self.tempHumedad[1]])
+                        else:
+                            self.tempHumedad = self._tyhQ.get()
+                            csv_out.writerow([self._celdaQ.get(),self._distQ.get(),"{:10.2f}".format(timestamp).strip(),self.tempHumedad[0], self.tempHumedad[1]])
+                        self._out.flush()
+                except Exception as e:
+                    if (type(e) == queue.Empty):
+                        pass
                     else:
-                        self.tempHumedad = self._tyhQ.get()
-                        csv_out.writerow([self._celdaQ.get(),self._distQ.get(),"{:10.2f}".format(timestamp).strip(),self.tempHumedad[0], self.tempHumedad[1]])
-                    self._out.flush()
-        
+                        msgBox = QMessageBox()
+                        msgBox.setIcon(QMessageBox.Critical)
+                        msgBox.setText('Error: ' + str(type(e)) + '\n'  + str(e))
+                        msgBox.setWindowTitle('Error')
+                        msgBox.exec_()
+                    
         finally:
+            self._out.flush()
             self._out.close()
 
     
     def __requestTemperatureAndHumidity(self):
-        while True:
-            if self._stopThreads:
-                break
-            self._lock.acquire()
-            self._serialArduino.write(b'<TMHM>\n')
-            humedad = self._serialArduino.readline().decode('ascii').strip()
-            temperatura = self._serialArduino.readline().decode('ascii').strip()
-            self._lock.release()
-            self._tyhQ.put((temperatura, humedad))
+        try:
+            while True:
+                if self._stopThreads:
+                    break
+                self._lock.acquire()
+                if (self.TEST_ENV):
+                    self._serialArduino.write(b'<TMHM>\n')
+                else:
+                    self._serialArduino.write(b'<TMHM>')
+                humedad = self._serialArduino.readline().decode('ascii').strip()
+                temperatura = self._serialArduino.readline().decode('ascii').strip()
+                self._lock.release()
+                self._tyhQ.put((temperatura, humedad))
+                time.sleep(60)
+        except Exception as e:
+            msgBox = QMessageBox()
+            msgBox.setIcon(QMessageBox.Critical)
+            msgBox.setText('Error: ' + str(type(e)) + '\n'  + str(e))
+            msgBox.setWindowTitle('Error')
+            msgBox.exec_()
 
-            time.sleep(60)
-
-    
-    
-    #legacy
-    def __storeData(self):
-        with open(self._path + '\\' + self._name + '.csv', 'w') as out:
-            csv_out = csv.writer(out)
-            #Write metada
-            csv_out.writerow(['#Nombre del experimento: {name}'.format(name=self._name)])
-            today = time.strftime('%d/%m/%y - %H:%M:%S', time.gmtime(self._t0))
-            csv_out.writerow(['#Fecha del experimento: {time}'.format(time=today)])
-
-            #Write data 
-            csv_out.writerow(['carga','distancia', 'tiempo'])
-            for row in self.data:
-                csv_out.writerow(row)
-
-
-#ensayo = Ensayo("Experiment", "500", "5", "5", "COM8", serial.Serial())
